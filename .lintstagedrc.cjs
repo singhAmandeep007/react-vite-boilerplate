6283235ff269d6bd94f23ec707094077f835a498
singhAmandeep007 <amandeeps0803@gmail.com>
2024-11-17 18:10:18 +0530

    chore: 🔧 added husky and lintstaged commands

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+  "src/**/*.{js,jsx,ts,tsx,css,scss,html,md,json}": () => ["npm run format", "npm run lint"],
+};
--- .lintstagedrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  "src/**/*.{js,jsx,ts,tsx,css,scss,html,md,json}": () => ["npm run tsc:check", "npm run lint", "npm run test"],
-};
