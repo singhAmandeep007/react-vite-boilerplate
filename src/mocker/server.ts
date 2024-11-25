8ade922c3d1e8334e2d65f329089630dc620e872
singhAmandeep007 <amandeeps0803@gmail.com>
2024-11-25 12:46:34 +0530

    fix: 🐛 msw service worker url on prod

--- src/mocker/server.ts
+++ src/mocker/server.ts
@@ -1,11 +1,17 @@
 import { setupWorker } from "msw/browser";
 
+import { isProduction } from "../utils";
 import { handlers } from "./handlers";
 
 const worker = setupWorker(...handlers);
 
+const baseUrl = isProduction ? "https://singhamandeep.me/react-vite-boilerplate/" : "/";
+
 export const runServer = () => {
   return worker.start({
     onUnhandledRequest: "bypass",
+    serviceWorker: {
+      url: `${baseUrl}mockServiceWorker.js`,
+    },
   });
 };
